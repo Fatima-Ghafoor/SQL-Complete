91cb00fe688f5bc3865dc0fb81822249a683d527
Fatima-Ghafoor <fatimaghafoor.tech@gmail.com>
2025-09-29 19:27:50 +0500

    Implement Aggregate Functions
    Ensured all Aggregate Functions (SUM, AVG, COUNT, MIN, MAX) are applied.

---
+++ Queries/07_AggregateFunctions.sql
@@ -0,0 +1,9 @@
+/* Select Database */
+USE RECORDS;
+
+/* ROW LIMITING (Table named StudentMarks has already been created in 04_Filtering.sql) */
+SELECT SUM(English) AS TotalEnglishMarks FROM StudentMarks;	/* SUM */
+SELECT AVG(History) AS AverageHistoryMarks FROM StudentMarks;	/* AVG */
+SELECT COUNT(Math) AS TotalMathCount FROM StudentMarks;	/* COUNT */
+SELECT MIN(English) AS MinimumEnglishMarks FROM StudentMarks;	/* MIN */
+SELECT MAX(History) AS MaximumHistoryMarks FROM StudentMarks;	/* MAX */
