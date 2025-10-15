c0a6b07d98c4b77ebabf1b889c10a7a0dc3392df
Fatima-Ghafoor <fatimaghafoor.tech@gmail.com>
2025-10-16 03:10:02 +0500

    More Functionalities Added
    Alter View (Add New Column, Change Expression Logic, Add More Columns for Display, Add Conditional Filter, Add an Alias), Select data from View, Check View of a Database.

--- Queries/13_Views.sql
+++ Queries/13_Views.sql
@@ -4,7 +4,7 @@ USE RECORDS;
 /* CREATE VIEWS */
 /* 1) Simple View */
 CREATE VIEW vw_StudentsAverage AS
-SELECT MarksID, (English + Math + History) / 3 AS AverageMarks FROM StudentMarks;
+SELECT MarksID, (English + Math) / 3 AS AverageMarks FROM StudentMarks;
 
 /* 2) Updateable View */
 CREATE VIEW vw_StudentsAge AS 
@@ -12,5 +12,34 @@ SELECT ID, Fname, Lname, Age FROM Students WHERE Age < 25;
 
 UPDATE vw_StudentsAge SET Age = 30;	/* Updating table through View */
 
+/* ALTER VIEW */
+/* 1) Add New Column */
+ALTER VIEW vw_StudentsAverage AS 
+SELECT MarksID, (English + Math + History) / 3 AS AverageMarks FROM StudentMarks;
+
+/* 2) Change Expression Logic */
+ALTER VIEW vw_StudentsAverage AS 
+SELECT MarksID, ROUND((English + Math + History) / 3.0, 2) AS AverageMarks FROM StudentMarks;
+
+/* 3) Add More Columns for Display */
+ALTER VIEW vw_StudentsAverage AS 
+SELECT MarksID, StudentID, English, Math, History, (English + Math + History) / 3 AS AverageMarks FROM StudentMarks;
+
+/* 4) Add Conditional Filter */
+ALTER VIEW vw_StudentsAverage AS 
+SELECT MarksID, (English + Math + History) / 3 AS AverageMarks FROM StudentMarks 
+WHERE (English + Math + History) / 3 <= 80;
+
+/* 5) Add an Alias */
+ALTER VIEW vw_StudentsAverage AS
+SELECT MarksID, (English + Math + History) / 3 AS Avg_Marks FROM StudentMarks;
+
+/* SELECT DATA FROM VIEW */
+SELECT * FROM vw_StudentsAverage;
+
 /* DROP VIEW */
 DROP VIEW vw_StudentsAverage;
+
+/* CHECK VIEWS OF A DATABASE */
+SELECT Name AS ViewName, object_id, create_date AS CreationDate, modify_date AS ModificationDate
+FROM sys.views;
