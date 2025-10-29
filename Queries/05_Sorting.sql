0cd85ddb33f7a2063bdd66891749598edf1a4ae0
Fatima-Ghafoor <fatimaghafoor.tech@gmail.com>
2025-10-29 17:20:44 +0500

    Refactor SQL sorting queries with improved comments
    Updated comments for clarity and added explanations for sorting methods.

--- Queries/05_Sorting.sql
+++ Queries/05_Sorting.sql
@@ -1,14 +1,14 @@
 /* Select Database */
 USE RECORDS;
 
-/* SORTING with ORDER BY (Table named Students has already been created in 01_Fundamentals.sql) */
-SELECT * FROM Students ORDER BY Lname ASC;	/* ASC */
-SELECT * FROM Students ORDER BY Fname DESC;	/* DESC */
-SELECT * FROM Students ORDER BY LEN(Fname);	/* EXPRESSION */
-SELECT ID, Fname, Lname, Major FROM Students ORDER BY 4;	/* COLUMN POSITION */
+/* Table named 'Students' has already been created in 01_Fundamentals.sql */
 
+/* SORTING with ORDER BY */
+/* 1) ASC */
+SELECT * FROM Students ORDER BY Lname ASC;	/* Sort result in Ascending Order (Small to Large) */
 
-/* ORDER BY can be used with more EXPRESSIONS like:
-1) String Functions: LEN(), UPPER(), LOWER(), LEFT(), RIGHT(), SUBSTRING(), REVERSE(), LTRIM(), RTRIM(), CONCAT();
-2) Numeric/Math Functions: ABS(), ROUND(), CEILIGN(), FLOOR(), POWER(), SQRT(), EXP(), LOG(), SIGN();
-3) Date/Time Functions: YEAR(), MONTH(), DAY(), DATENAME(), DATEPART(), GETDATE(); */
+/* 2) DESC */
+SELECT * FROM Students ORDER BY Fname DESC;	/* Sort result in Descending Order (Large to Small) */
+
+/* 3) COLUMN POSITION */
+SELECT ID, Fname, Lname, Major FROM Students ORDER BY 4;	/* Sorts result based on the column’s numeric position */
