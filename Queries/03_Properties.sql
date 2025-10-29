000357bf39d76799d1c13880042cc02f41213a2b
Fatima-Ghafoor <fatimaghafoor.tech@gmail.com>
2025-10-29 17:24:24 +0500

    Minor changes with improved readability

--- Queries/03_Properties.sql
+++ Queries/03_Properties.sql
@@ -16,23 +16,25 @@ CREATE TABLE StudentActivities(
 INSERT INTO StudentActivities (StudentID, Sports, Trophies, Team) VALUES (1, 'Football', 2, 'A'), (3, 'Football', 1, 'A'), (2, 'Cricket', 0, 'B');
 
 /* ADD PROPERTIES WHILE ALTERING -> when Columns already exist */
-/* 1) Identity */
-ALTER TABLE StudentActivities ALTER COLUMN Atvs_ID PRIMARY KEY (ActivityID);
+/* 1) IDENTITY */
+ALTER TABLE StudentActivities ALTER COLUMN Atvs_ID PRIMARY KEY (ActivityID);	/* Automatically generates sequential numeric values for a column */
 
-/* 2) Not Null */
-ALTER TABLE StudentActivities ALTER COLUMN Sports varchar(30) NOT NULL;
+/* 2) NOT NULL */
+ALTER TABLE StudentActivities ALTER COLUMN Sports varchar(30) NOT NULL;	/* Ensure a column cannot store Null values */
 
-/* 3) Null */
-ALTER TABLE StudentActivities ALTER COLUMN Trophies int NULL;
+/* 3) NULL */
+ALTER TABLE StudentActivities ALTER COLUMN Trophies int NULL;	/* Allows a column to store Null values */
 
-/* 4) Collate */
+/* 4) COLLATE */
 ALTER TABLE StudentActivities ALTER COLUMN Team varchar(30) COLLATE SQL_Latin1_General_CP1_CS_AS; /* Case Sensitive, Accent Sensitive */
 ALTER TABLE StudentActivities ALTER COLUMN Team varchar(30) COLLATE SQL_Latin1_General_CP1_CS_AI; /* Case Sensitive, Accent Insensitive */
 ALTER TABLE StudentActivities ALTER COLUMN Team varchar(30) COLLATE SQL_Latin1_General_CP1_CI_AS; /* Case Insensitive, Accent Sensitive */
 ALTER TABLE StudentActivities ALTER COLUMN Team varchar(30) COLLATE SQL_Latin1_General_CP1_CI_AI; /* Case Insensitive, Accent Insensitive */
+/* Defines the sorting and comparison rules for text data */
 
-/* 5) Computed Column */
+/* 5) COMPUTED COLUMN */
 ALTER TABLE StudentActivities ALTER COLUMN Summary AS (Sports COLLATE SQL_Latin1_General_CP1_CS_AS + ' - ' + Team COLLATE SQL_Latin1_General_CP1_CS_AS);
+/* Displays values calculated from other columns in the same table */
 
-/*Search using COLLATE*/
+/* SEARCH using COLLATE */
 SELECT * FROM StudentActivities WHERE Team COLLATE SQL_Latin1_General_CP1_CS_AS = 'A';
