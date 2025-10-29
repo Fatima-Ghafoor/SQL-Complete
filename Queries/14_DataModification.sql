0057ed4c11e3b386b66187b1b699eae1692f2cdf
Fatima-Ghafoor <fatimaghafoor.tech@gmail.com>
2025-10-29 17:16:31 +0500

    Sequential number update & minor changes.

--- Queries/14_DataModification.sql
+++ Queries/14_DataModification.sql
@@ -1,8 +1,10 @@
 /* Select Database */
 USE RECORDS;
 
-/* UPDATE TABLE DATA (Tables named Students & StudentsData have already been created in 01_Fundamentals.sql & 09_Joins.sql) */
-/* 1) Basic */
+/* Tables named 'Students' & 'StudentsData' have already been created in 01_Fundamentals.sql & 13_Joins.sql */
+
+/* UPDATE TABLE DATA */
+/* 1) BASIC */
 UPDATE Students SET Age = 25;
 
 /* 2) WHERE */
@@ -15,7 +17,7 @@ FROM Students s
 INNER JOIN StudentsData sd
 ON s.ID = sd.ID;
 
-/* 4) Subquery */
+/* 4) SUBQUERY */
 UPDATE Students SET Major = (Select Hobby from StudentsData Where ID = 2) WHERE ID = 1;
 
 /* 5) OUTPUT */
@@ -24,31 +26,31 @@ OUTPUT deleted.ID, deleted.Age AS OldAge, inserted.Age AS NewAge
 WHERE ID = 1;
 
 /* DELETE TABLE DATA */
-/* 1) Basic */
+/* 1) BASIC */
 DELETE FROM Students;	/* --similar to Truncate */
 
 /* 2) WHERE */
 DELETE FROM Students WHERE ID = 1;
 
 /* 3) JOIN */
-/* This deletes from the Students, but only those who have a matching record in StudentsData with Age < 18. */
 DELETE s
 FROM Students s
 INNER JOIN StudentsData sd
 ON s.ID = sd.ID
 WHERE sd.Age < 18;
+/* This deletes from the Students, but only those who have a matching record in StudentsData with Age < 18 */
 
-/* This deletes from Students where no matching record exists in StudentsData */
 DELETE s
 FROM Students s
 LEFT JOIN StudentsData sd
 ON s.ID = sd.ID
 WHERE sd.ID IS NULL;
+/* This deletes from Students where no matching record exists in StudentsData */
 
-/* --use INNER JOIN for common records and LEFT JOIN for records that only exist in the targeted table */
+/* Use INNER JOIN for common records and LEFT JOIN for records that only exist in the targeted table */
 
-/* 4) Subquery */
-DELETE FROM Students WHERE ID IN (Select ID from StudentsData);	/*  --will only remove Students who exist in StudentsData */
+/* 4) SUBQUERY */
+DELETE FROM Students WHERE ID IN (Select ID from StudentsData);	/*  Only remove Students who exist in StudentsData */
 
 /* 5) OUTPUT */
 DELETE FROM Students
@@ -56,4 +58,4 @@ OUTPUT deleted.ID, deleted.Fname, deleted.Lname
 WHERE Age < 18;
 
 /* TRUNCATE TABLE */
-TRUNCATE TABLE Students;	/* --delete all records from table */
+TRUNCATE TABLE Students;	/* Delete all records from table */
