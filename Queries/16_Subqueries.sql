a0425ac69c0d75aabfac93cf2c65c9250b895319
Fatima-Ghafoor <fatimaghafoor.tech@gmail.com>
2025-10-29 17:14:13 +0500

    Sequential number update & minor changes.

--- Queries/16_Subqueries.sql
+++ Queries/16_Subqueries.sql
@@ -1,11 +1,13 @@
 /* Select Database */
 USE RECORDS;
 
-/* SUBQUERIES (Tables named StudentActivities & StudentMarks has already been created in 03_Properties & 04_Filtering.sql) */
-/* 1) Scalar Subquery */
+/* Tables named 'StudentActivities' & 'StudentMarks' have already been created in 03_Properties.sql & 04_Filtering.sql */
+
+/* SUBQUERIES */
+/* 1) SCALAR SUBQUERY */
 SELECT MarksID, Math, (Select AVG(Math) from StudentMarks) AS ClassMathAvg FROM StudentMarks;
 
-/* 2) Multi-row Subquery */
+/* 2) MULTI-ROW SUBQUERY */
 SELECT MarksID, English, Math, History FROM StudentMarks WHERE StudentID IN 
 (Select StudentID from StudentActivities Where Sports = 'Football');	/* IN */
 
@@ -15,7 +17,7 @@ SELECT MarksID, Math FROM StudentMarks WHERE Math > ANY
 SELECT MarksID, Math FROM StudentMarks WHERE History > ALL 
 (Select Trophies from StudentActivities Where Trophies is Not Null);	/* ALL */
 
-/* 3) Correlated Subquery */
+/* 3) CORRELATED SUBQUERY */
 SELECT MarksID, Math FROM StudentMarks s1 WHERE Math > 
 (Select AVG(Math) from StudentMarks s2 Where s2.MarksID <> s1.MarksID);
-/* --will show the students whose marks are greater than the average marks of everyone except themselves. */
+/* Show the students whose marks are greater than the average marks of everyone except themselves */
