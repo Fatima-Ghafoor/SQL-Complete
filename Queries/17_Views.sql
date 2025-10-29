7a7ec981037c30963ffc604af0eef88a24f6bad9
Fatima-Ghafoor <fatimaghafoor.tech@gmail.com>
2025-10-29 17:13:31 +0500

    Sequential number update & minor changes.

--- Queries/17_Views.sql
+++ Queries/17_Views.sql
@@ -1,36 +1,38 @@
 /* Select Database */
 USE RECORDS;
 
+/* Table named 'StudentMarks' has already been created in 04_Filtering.sql */
+
 /* CREATE VIEWS */
-/* 1) Simple View */
+/* 1) SIMPLE VIEW */
 CREATE VIEW vw_StudentsAverage AS
 SELECT MarksID, (English + Math) / 3 AS AverageMarks FROM StudentMarks;
 
-/* 2) Updateable View */
+/* 2) UPDATEABLE VIEW */
 CREATE VIEW vw_StudentsAge AS 
 SELECT ID, Fname, Lname, Age FROM Students WHERE Age < 25;
 
 UPDATE vw_StudentsAge SET Age = 30;	/* Updating table through View */
 
 /* ALTER VIEW */
-/* 1) Add New Column */
+/* 1) ADD NEW COLUMN */
 ALTER VIEW vw_StudentsAverage AS 
 SELECT MarksID, (English + Math + History) / 3 AS AverageMarks FROM StudentMarks;
 
-/* 2) Change Expression Logic */
+/* 2) CHANGE EXPRESSION LOGIC */
 ALTER VIEW vw_StudentsAverage AS 
 SELECT MarksID, ROUND((English + Math + History) / 3.0, 2) AS AverageMarks FROM StudentMarks;
 
-/* 3) Add More Columns for Display */
+/* 3) ADD MORE COLUMNS FOR DISPLAY */
 ALTER VIEW vw_StudentsAverage AS 
 SELECT MarksID, StudentID, English, Math, History, (English + Math + History) / 3 AS AverageMarks FROM StudentMarks;
 
-/* 4) Add Conditional Filter */
+/* 4) ADD CONDITIONAL FILTER */
 ALTER VIEW vw_StudentsAverage AS 
 SELECT MarksID, (English + Math + History) / 3 AS AverageMarks FROM StudentMarks 
 WHERE (English + Math + History) / 3 <= 80;
 
-/* 5) Add an Alias */
+/* 5) ADD AN ALIAS */
 ALTER VIEW vw_StudentsAverage AS
 SELECT MarksID, (English + Math + History) / 3 AS Avg_Marks FROM StudentMarks;
 
