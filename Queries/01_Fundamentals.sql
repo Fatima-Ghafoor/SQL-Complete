af4ccca02e9b01fe1e6c99702de77e4b163056c9
Fatima-Ghafoor <fatimaghafoor.tech@gmail.com>
2025-10-29 17:25:29 +0500

    Minor changes with improved readability
    Updated SQL comments for clarity and consistency.

--- Queries/01_Fundamentals.sql
+++ Queries/01_Fundamentals.sql
@@ -19,37 +19,37 @@ CREATE TABLE Students(
 );
 
 /* ALTER TABLE */
-/* 1) Add Column */
+/* 1) ADD COLUMN */
 ALTER TABLE Students ADD Email varchar(50);
 
-/* 2) Drop Column */
+/* 2) DROP COLUMN */
 ALTER TABLE Students DROP COLUMN Email;
 
-/* 3) Modify Datatype or Size */
+/* 3) MODIFY DATATYPE OR SIZE */
 ALTER TABLE Students ALTER COLUMN Major varchar(50);
-
-/* INSERT DATA INTO TABLE */
-INSERT INTO Students (Fname, Lname, Major, Age, Email) VALUES ('Fahad','Khalid','Physics', 22, 'fahad@gmail.com'), ('Sara','Nasir','Mechanical', 24, 'sara@gmail.com'), ('Umer','Khan', 'Electronics', 23, 'umer@gmail.com'), ('Zainab','Amir','Mathematics', 22, 'zainab@gmail.com'), ('Fariha','Asim','Mathematics', 24, 'fariha@gmail.com');
-
-/* SELECT DATA FROM TABLE */
-/* 1) All Columns */
-SELECT * FROM Students;
-
-/* 2) Selected Columns */
-SELECT Fname, Lname FROM Students;
-
-/* COLUMN ALIASES -> (writing AS is optional) */
-/* 1) Single Column */
-SELECT Fname AS FirstName FROM Students;
-
-/* 2) Multiple Columns */
-SELECT Fname AS FirstName, Lname AS LastName FROM Students;
-
-/* 3) Merging Columns */
-SELECT Fname + ' ' + Lname AS FullName FROM Students;
 
 /* DROP TABLE */
 DROP TABLE Students;
 
-/* TABLE ALIASE */
+/* INSERT DATA INTO TABLE */
+INSERT INTO Students (Fname, Lname, Major, Age, Email) VALUES ('Fahad', 'Khalid', 'Physics', 22, 'fahad@gmail.com'), ('Sara', 'Nasir', 'Mechanical', 24, 'sara@gmail.com'), ('Umer', 'Khan', 'Electronics', 23, 'umer@gmail.com'), ('Zainab', 'Amir', 'Mathematics', 22, 'zainab@gmail.com'), ('Fariha', 'Asim', 'Mathematics', 24, 'fariha@gmail.com');
+
+/* SELECT DATA FROM TABLE */
+/* 1) ALL COLUMNS */
+SELECT * FROM Students;
+
+/* 2) SELECTED COLUMNS */
+SELECT Fname, Lname FROM Students;
+
+/* TABLE ALIAS -> (writing AS is optional) */
 SELECT s.Fname, s.Lname FROM Students AS s;
+
+/* COLUMN ALIASES -> (writing AS is optional) */
+/* 1) SINGLE COLUMN */
+SELECT Fname AS FirstName FROM Students;
+
+/* 2) MULTIPLE COLUMNS */
+SELECT Fname AS FirstName, Lname AS LastName FROM Students;
+
+/* 3) MERGING COLUMNS */
+SELECT Fname + ' ' + Lname AS FullName FROM Students;
